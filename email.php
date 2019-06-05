c7b6e5b7bca0cf2eafdbb717b46dd10daae270a4
João Pedro <jonhpyter2006@gmail.com>
2019-06-05 16:32:31 -0300

    AJUSTES DE TELA PAISAGEM

--- email.php
+++ email.php
@@ -2,16 +2,19 @@
 $Nome		= $_POST["Nome"];	// Pega o valor do campo Nome
 $Fone		= $_POST["Fone"];	// Pega o valor do campo Telefone
 $Email		= $_POST["Email"];	// Pega o valor do campo Email
+$Imagem	= $_POST["Imagem"];	// Pega os valores do campo Mensagem
+$Curriculo	= $_POST["Curriculo"];	// Pega os valores do campo Mensagem
 $Mensagem	= $_POST["Mensagem"];	// Pega os valores do campo Mensagem
+
 
 // Variável que junta os valores acima e monta o corpo do email
 
-$Vai = "Nome: $Nome\n\nE-mail: $Email\n\nTelefone: $Fone\n\nMensagem: $Mensagem\n";
+$Vai = "Nome: $Nome\n\nE-mail: $Email\n\nTelefone: $Fone\n\nFoto: $Imagem\n\nCurrículo: $Curriculo\n\nMensagem: $Mensagem\n";
 
 require_once("phpmailer/class.phpmailer.php");
 
-define('GUSER', 'jonhpyter2006@gmail.com');	// <-- Insira aqui o seu GMail
-define('GPWD', 'Jonny2006@');		// <-- Insira aqui a senha do seu GMail
+define('GUSER', 'suporte@usinabomjesus.com.br');	// <-- Insira aqui o seu GMail
+define('GPWD', 'ubj821371');		// <-- Insira aqui a senha do seu GMail
 
 function smtpmailer($para, $de, $de_nome, $assunto, $corpo) { 
 	global $error;
@@ -19,8 +22,8 @@ function smtpmailer($para, $de, $de_nome, $assunto, $corpo) {
 	$mail->IsSMTP();		// Ativar SMTP
 	$mail->SMTPDebug = 0;		// Debugar: 1 = erros e mensagens, 2 = mensagens apenas
 	$mail->SMTPAuth = true;		// Autenticação ativada
-	$mail->SMTPSecure = 'ssl';	// SSL REQUERIDO pelo GMail
-	$mail->Host = 'smtp.gmail.com';	// SMTP utilizado
+	$mail->SMTPSecure = 'tls';	// SSL REQUERIDO pelo GMail
+	$mail->Host = 'smtp.terra.com.br';	// SMTP utilizado
 	$mail->Port = 587;  		// A porta 587 deverá estar aberta em seu servidor
 	$mail->Username = GUSER;
 	$mail->Password = GPWD;
@@ -39,7 +42,7 @@ function smtpmailer($para, $de, $de_nome, $assunto, $corpo) {
 
 // Insira abaixo o email que irá receber a mensagem, o email que irá enviar (o mesmo da variável GUSER), o nome do email que envia a mensagem, o Assunto da mensagem e por último a variável com o corpo do email.
 
- if (smtpmailer('unifytelecom1@gmail.com', 'jonhpyter2006@gmail.com', 'Nome do Enviador', 'Assunto do Email', $Vai)) {
+ if (smtpmailer('jonhpyter2006@gmail.com', 'suporte@usinabomjesus.com.br', 'Nome do Enviador', 'Assunto do Email', $Vai)) {
 
 	Header("location:https://jpmcavalcante.github.io/agenciaAPromo"); // Redireciona para uma página de obrigado.
 
